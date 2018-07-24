de43fa6fa8e4ce68ba491853fbbfcfa4177d7955
sotojang <mrjang28@gmail.com>
2018-07-24 11:36:48 +0900

    first commit

---
+++ client/main.js
@@ -0,0 +1,20 @@
+
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+/**
+* Created by Soto
+* Copyright @ SotoLAB Co,.Ltd
+*/ 
+
+Template.listdetail.onCreated(function listdetailOnCreated() {
+ var address = FlowRouter.getParam('name');
+ Meteor.subscribe('mywallet', address);
+});
+
+Template.listdetail.rendered = function () {
+ var address = FlowRouter.getParam('name');
+ console.log("We are on the post: ", address);
+}
